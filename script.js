def1d74af80ebffe9dea7bcddc5a2cdcc62ec523
JnetSilvestre <jvsilvestre01@gmail.com>
2024-05-20 22:07:14 -0300

    visual finalizado + 50% funcional

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-const carrossel = document.querySelector(".carrossel");
+const controls = document.querySelectorAll(".control");
 
-const dragging = (e) => {
-    console.log("")
-}
+let currentBloco = 0;
 
-carrossel.addEventListener("mousemove", daragging);
+const blocos = document.querySelectorAll(".blocoCarrossel");
+
+const maxBlocos = blocos;
+
+controls.forEach(control => {
+    control.addEventListener("click", () => {
+        const isLeft = control.classList.contains("arrowLeft");
+
+        if (isLeft) {
+            currentBloco -= 1;
+        } else {
+            currentBloco += 1;
+        }
+
+        if (currentBloco >= maxBlocos) {
+            currentBloco = 0;
+        }
+        if (currentBloco < 0) {
+            currentBloco = maxBlocos - 1;
+        }
+
+        blocos.forEach(blocoCarrossel => blocoCarrossel.classList.remove("current-bloco"));
+
+        blocos[currentBloco].scrollIntoView({
+            inline: "center",
+            behavior: "smooth",
+        });
+
+        blocos[currentBloco].classList.add(current - bloco);
+
+    });
+});
+
